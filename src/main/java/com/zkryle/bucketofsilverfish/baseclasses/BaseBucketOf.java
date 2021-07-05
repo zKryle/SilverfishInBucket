ae29bebfb6b55702d04f623792e71890e084fe3d
zKryle <ciro20797@gmail.com>
2021-07-05 11:19:39 +0200

    first commit

--- src/main/java/com/zkryle/bucketofsilverfish/baseclasses/BaseBucketOf.java
+++ src/main/java/com/zkryle/bucketofsilverfish/baseclasses/BaseBucketOf.java
@@ -1,6 +1,5 @@
 package com.zkryle.bucketofsilverfish.baseclasses;
 
-import com.zkryle.bucketofsilverfish.core.ItemInit;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.monster.SilverfishEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,9 +7,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 public class BaseBucketOf extends Item{
@@ -40,18 +42,25 @@ public class BaseBucketOf extends Item{
     }
 
     @Override
-   public ActionResult <ItemStack> use( World level , PlayerEntity player , Hand hand ){
-        BlockPos pos = getPlayerPOVHitResult( level , player , RayTraceContext.FluidMode.NONE ).getBlockPos();
-        if (player.getMainHandItem().getItem() != Items.BUCKET.getItem() && !player.getMainHandItem().isEmpty()){
+    public ActionResult <ItemStack> use( World level , PlayerEntity player , Hand hand ){
+        RayTraceResult rtresult = getPlayerPOVHitResult( level , player , RayTraceContext.FluidMode.NONE );
+        BlockRayTraceResult brtresult = (BlockRayTraceResult) rtresult;
+        BlockPos pos = brtresult.getBlockPos();
+        Direction direction = brtresult.getDirection();
+        BlockPos pos1 = pos.relative(direction);
+        ItemStack itemstack = player.getItemInHand(hand);
+        if (level.mayInteract(player, pos) && player.getItemInHand( hand ) != Items.BUCKET.getDefaultInstance() && player.mayUseItemAt( pos1, direction, itemstack )){
             switch(type){
                 case "silverfish":
-                    if(player.getMainHandItem().getItem() == ItemInit.BUCKET_OF_SILVERFISH.get()){
-                        SilverfishEntity entity = new SilverfishEntity( EntityType.SILVERFISH , level );
-                        entity.setPos( pos.getX() - 0.2f , pos.getY() + 1 , pos.getZ() - 0.2f );
+                    SilverfishEntity entity = new SilverfishEntity( EntityType.SILVERFISH, level);
+                    entity.setPos( pos.getX()+0.2f, pos.getY()+1, pos.getZ()+0.2f);
+                    if (!level.isClientSide()){
                         level.addFreshEntity( entity );
+                        return ActionResult.success( Items.BUCKET.getDefaultInstance() );
                     }
+                    break;
             }
         }
-        return ActionResult.sidedSuccess( !player.abilities.instabuild ? new ItemStack( Items.BUCKET ) : new ItemStack( ItemInit.BUCKET_OF_SILVERFISH.get() ) , level.isClientSide() );
+        return ActionResult.pass( itemstack );
     }
 }
--- src/main/java/com/zkryle/bucketofsilverfish/events/ModEvents.java
+++ src/main/java/com/zkryle/bucketofsilverfish/events/ModEvents.java
@@ -1,30 +1,34 @@
 package com.zkryle.bucketofsilverfish.events;
 
+import com.zkryle.bucketofsilverfish.baseclasses.BaseBucketOf;
 import com.zkryle.bucketofsilverfish.core.ItemInit;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EndermiteEntity;
 import net.minecraft.entity.monster.SilverfishEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ModEvents{
 
-    private static boolean used = false;
+    private void fillBucket( ItemStack stack , PlayerEntity player , Entity entity, World level, Hand hand ){
+        entity.remove();
+        player.playSound( SoundEvents.BUCKET_FILL_FISH , 1.0f , 1.0f );
+        player.setItemInHand( hand, stack );
+    }
 
     @SubscribeEvent
     public void onRightClick( PlayerInteractEvent.EntityInteract event ){
-        if(event.getItemStack().getItem() == Items.BUCKET.getItem()){
+        if(event.getPlayer().getItemInHand( event.getHand() ).getItem() == Items.BUCKET.getItem()){
             if(event.getTarget().getEntity() instanceof SilverfishEntity){
-                event.getTarget().remove();
-                event.getPlayer().playSound( SoundEvents.BUCKET_FILL_FISH, 1.0f, 1.0f );
-                event.getPlayer().inventory.setItem( event.getPlayer().inventory.selected ,
-                        ItemInit.BUCKET_OF_SILVERFISH.get().getDefaultInstance() );
-            } else if (event.getTarget().getEntity() instanceof EndermiteEntity){
-                event.getTarget().remove();
-                event.getPlayer().playSound( SoundEvents.BUCKET_FILL_FISH, 1.0f, 1.0f );
-                event.getPlayer().inventory.setItem( event.getPlayer().inventory.selected ,
-                        ItemInit.BUCKET_OF_ENDERMITE.get().getDefaultInstance() );
+                    fillBucket( ItemInit.BUCKET_OF_SILVERFISH.get().getDefaultInstance() , event.getPlayer() , event.getTarget(), event.getWorld(), event.getHand() );
+            }else if(event.getTarget().getEntity() instanceof EndermiteEntity){
+                fillBucket( ItemInit.BUCKET_OF_ENDERMITE.get().getDefaultInstance() , event.getPlayer() , event.getTarget(), event.getWorld(), event.getHand());
             }
         }
     }
